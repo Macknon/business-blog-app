abf54a32e58b81688baa9326a59176094fcdd625
Injenga Macknon Korongo <macknoninjenga2000@gmail.com>
2022-10-12 15:04:48 +0300

    route mod

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\BizNewsController;
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+Route::get('/news', [BizNewsController::class, 'index'])->name('news.index');
+
+Route::get('/news/selected-news', [BizNewsController::class, 'show'])->name('news.show');
+
+Route::get('/about', function () { return view('about'); })->name('about');
+
+Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
