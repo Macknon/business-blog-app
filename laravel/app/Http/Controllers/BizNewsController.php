d79ffe75677ea47bfceda12710c667f43289f1f6
Injenga Macknon Korongo <macknoninjenga2000@gmail.com>
2022-10-15 21:29:59 +0300

    controller for news

--- laravel/app/Http/Controllers/BizNewsController.php
+++ laravel/app/Http/Controllers/BizNewsController.php
@@ -3,13 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class BizNewsController extends Controller
 {
     public function index(){
-        return view('news');
+        return view('allNews.news');
+    }
+    public function create(){
+        return view('allNews.add-news');
+    }
+    public function store(Request $request){
+        $request->validate([
+            'title' => 'required',
+            'image' => 'required | image',
+            'highlight' => 'required',
+            'body' => 'required'
+        ]);
+        dd('val P put I');
+        $title = $request->input('title');
+        $slug = Str::slug($title, '-');
+        $user_id = Auth::user()->id;
+        $body = $request->input('body');
+
+        $request->file('image')->store('imgs', 'public');
+        dd($title);
     }
     public function show(){
-        return view('selected-news');
+        return view('allNews.selected-news');
     }
 }
