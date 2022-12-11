17c40b4acb475c70d2936e1f11ef96b1295f0a40
Injenga Macknon Korongo <macknoninjenga2000@gmail.com>
2022-12-11 13:29:00 +0300

    views for mail

---
+++ resources/views/mail/mail.blade.php
@@ -0,0 +1,9 @@
+
+    <p><b>Name of sender: {{$data['name']}} </b></p>
+    <p>Email: {{$data['email']}}</p>
+    <div style="margin-top: 20px;">
+<h3>New message:</h3>
+<p>{{$data['message']}}</p>
+
+
+</div>
