c8b60023bce0c70a12517472e872847c375c11df
Injenga Macknon Korongo <macknoninjenga2000@gmail.com>
2022-12-12 16:56:01 +0300

    mods ,in design

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,9 +1,9 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-    <div>
-        {{ $logo }}
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+    <div style="font-size: x-large">
+        Hello!
     </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <div class="w-full sm:max-w-md mt-6 px-6 py-6 shadow-md overflow-hidden sm:rounded-lg">
         {{ $slot }}
     </div>
 </div>
